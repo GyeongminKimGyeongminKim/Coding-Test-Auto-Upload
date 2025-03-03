96625af1d1ffaa24c4dc7d72198d535b206cec75
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-03-04 02:44:08 +0900

    [level 0] Title: 배열 두배 만들기, Time: 8.96 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.kt
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -1,9 +1,5 @@
 class Solution {
     fun solution(numbers: IntArray): IntArray {
-        var answer: IntArray = numbers
-        for (i in 0 until answer.size) {
-            answer[i] = answer[i] * 2
-        }
-        return answer
+        return numbers.map { it * 2 }.toIntArray()
     }
 }
