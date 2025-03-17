4ffeb9eb15e23af18f12d83822710a1fcd7b6646
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-03-18 02:48:25 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.kt
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.kt
@@ -1,16 +1,17 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var pizzaNum: Int
-        var slice = 6
-        if(n%slice == 0) {
-            pizzaNum = n/slice
-        } else {
-            pizzaNum = 1
-            while(slice%n != 0) {
-                slice += 6
-                pizzaNum += 1
-            }
+    fun gcd(a: Int, b: Int): Int {
+        var x: Int = a
+        var y: Int = b
+        while(y != 0) {
+            val temp = y
+            y = x%y
+            x = temp
         }
-        return pizzaNum
+        return x
+    }
+    
+    fun solution(n: Int): Int {
+        val pizza = gcd(n, 6)
+        return n*6/pizza/6
     }
 }
